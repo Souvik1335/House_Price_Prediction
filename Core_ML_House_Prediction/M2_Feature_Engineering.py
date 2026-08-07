798e228a4e4cea7e8c364c2cbe975ad3d0bb6222
Souvik1335 <souvikbanerjee1335@gmail.com>
2026-08-07 13:34:33 +0530

    Feature Engineering for this Model

--- Core_ML_House_Prediction/M2_Feature_Engineering.py
+++ Core_ML_House_Prediction/M2_Feature_Engineering.py
@@ -1,16 +1,31 @@
 from M1_House_Price_Data import df
 
+
+def create_engineered_features(df):
+
+    df = df.copy()
+
+    # Feature Engineering
+    df["Area_per_Bedroom"] = df["Area_sqft"] / df["Bedrooms"]
+
+    df["Bathroom_per_Bedroom"] = (
+        df["Bathrooms"] / df["Bedrooms"]
+    )
+
+    return df
+
+
 # Delete House ID column
-df = df.drop(columns=['HouseID'])
+df = df.drop(columns=["HouseID"])
 
-# Feature Engineering
-df['Area_per_Bedroom'] = df['Area_sqft'] / df['Bedrooms']
+# Apply Feature Engineering
+df = create_engineered_features(df)
 
-df['Bathroom_per_Bedroom'] = (df['Bathrooms'] / df['Bedrooms'])
 
 # Separate features and target
 X = df.drop(columns=["Price"])
 y = df["Price"]
+
 
 print(X.head())
 print(X.columns)
